0f5ffea1dfec86b5e455c4ae54cbeb1a46d134e7
Envy49-bit <envy49c9@gmail.com>
2026-04-28 23:50:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
 </head>
 <body>
 
-<h1>Bienvenido a Refacciones Italika</h1>
+<h1>Bienvenido a Papeleria Tony</h1>
 
-<p>Venta de refacciones para motocicletas.</p>
+<p>Venta de articulos para oficinas.</p>
 
 <h2>Misión</h2>
 <p>Ofrecer productos de calidad al mejor precio.</p>
 
 <h2>Visión</h2>
-<p>Ser líderes en distribución de refacciones.</p>
+<p>Ser líderes en distribución de articulos</p>
 
 <a href="login.php">Ingresar al sistema</a>
 
 <hr>
 <footer>
-    <p>© 2026 Refacciones Italika</p>
+    <p>© 2026 Papeleria Tony</p>
 </footer>
 
 </body>
