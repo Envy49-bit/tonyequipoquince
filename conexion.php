b31abb424f213929dab51dcfed3d81518fa5f1b1
Envy49-bit <envy49c9@gmail.com>
2026-04-28 23:40:48 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,12 @@
+<?php
+$servidor = "localhost";
+$usuario = "dev_user";
+$password = "Dev*2026";
+$bd = "tonyequipoquince";
 
+$conexion = new mysqli($servidor, $usuario, $password, $bd);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+?>
