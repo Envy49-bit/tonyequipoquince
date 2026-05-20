c3b3504d343c452c6fdc766d844bc66a91f4034b
Envy49-bit <envy49c9@gmail.com>
2026-05-19 18:56:32 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $servidor = "localhost";
 $usuario = "dev_user";
 $password = "Dev*2026";
-$bd = "tonyequipoquince";
+$bd = "papeleriatonyequipoquince";
 
 $conexion = new mysqli($servidor, $usuario, $password, $bd);
 
